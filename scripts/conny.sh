6339600ad12efedc455b07d7d59430a7fc593962
Ian Castleden <ian.castleden@uwa.edu.au>
2025-07-04 01:16:53 +0800

    OLD_PS1

--- scripts/conny.sh
+++ scripts/conny.sh
@@ -1,5 +1,6 @@
-CONNY_ENV_LOCATION=. # /mnt/s-ws/env
-environment='scie4002'
+CONNY_ENV_LOCATION=${CONNY_ENV_LOCATION:-/mnt/s-ws/env}
+environment=scie4002
+
 function __conny_hashr() {
     if [ -n "${ZSH_VERSION:+x}" ]; then
         \rehash
@@ -20,7 +21,7 @@ function conny () {
                 return
             fi;
             export CONNY_OLD_PATH="${PATH}"
-            OPS1="$PS1"
+            OLD_PS1="$PS1"
             PS1="(\033[33m\033[1mconny\033[0m) $PS1"
             source "$CONNY_ENV_LOCATION/${environment}-activate.sh"
             __conny_hashr
@@ -30,9 +31,9 @@ function conny () {
                 source "$CONNY_ENV_LOCATION/${environment}-deactivate.sh"
                 export PATH="${CONNY_OLD_PATH}"
                 unset CONNY_OLD_PATH
-                if [[ -v OPS1 ]]; then
-                    PS1="$OPS1"
-                    unset OPS1
+                if [[ -v OLD_PS1 ]]; then
+                    PS1="$OLD_PS1"
+                    unset OLD_PS1
                 fi
                 __conny_hashr
             else
@@ -41,7 +42,6 @@ function conny () {
             ;;
         *) 
             echo -e '\033[31m\033[1munknown argument use: "conny activate|deactivate"\033[0m' 1>&2;
-            
             ;;
     esac
 
--- scripts/slurm.py
+++ scripts/slurm.py
@@ -10,6 +10,7 @@ import shlex
 import click
 from typing import NoReturn
 from contextlib import suppress
+from uuid import uuid4
 
 
 def run(cmd: list[str]) -> None:
@@ -27,27 +28,38 @@ def error(msg: str) -> NoReturn:
 
 
 @click.command(
-    context_settings=dict(ignore_unknown_options=True, allow_extra_args=True)
+    context_settings=dict(ignore_unknown_options=True, 
+                          allow_extra_args=True)
 )
 @click.option("--slurm-sleep", "sleep", default=10.0)
-@click.option("--job-name")
+@click.option("--job-name", help='give a name to this job')
+@click.option("--script", help='argument is a script')
 @click.pass_context
-def slurm(ctx, sleep:float, job_name:str | None):
+def slurm(ctx, sleep:float, job_name:str | None, script:bool):
     sbatch = which("sbatch")
     if sbatch is None:
-        error("slurm not installed!")
+        error("slurm is not installed!")
     if not ctx.args:
         error("no command to run!")
-    slurmsh = '.slurm.sh'
-    with open(slurmsh, "wt", encoding="utf8") as fp:
-        print("#!/bin/bash", file=fp)
-        print(f"sleep {sleep}", file=fp)
-        print(shlex.join(ctx.args), file=fp)
+    if script:
+        slurmsh = ctx.args[0]
+        if not os.path.isfile(slurmsh):
+            error(f"{slurmsh} is not a file!")
+    
+        remove = False
+    else:
+        slurmsh = f'.slurm-{uuid4()}.sh'
+        remove = True
+        with open(slurmsh, "wt", encoding="utf8") as fp:
+            print("#!/bin/bash", file=fp)
+            print(f"sleep {sleep}", file=fp)
+            print(shlex.join(ctx.args), file=fp)
 
     if job_name is None:
         for a in ctx.args:
-            job_name = a
-            break
+            if not a.startswith('-'):
+                job_name = a
+                break
     try:
         cmds = [sbatch]
         if job_name:
@@ -56,8 +68,9 @@ def slurm(ctx, sleep:float, job_name:str | None):
 
         run(cmds)
     finally:
-        with suppress(OSError, FileNotFoundError):
-            os.remove(slurmsh)
+        if remove:
+            with suppress(OSError, FileNotFoundError):
+                os.remove(slurmsh)
 
 
 if __name__ == "__main__":
