e3cdb09c793bfc889aa15581c037fb6f1161cbb9
Ian Castleden <ian.castleden@uwa.edu.au>
2025-07-05 14:42:19 +0800

    no - as command

--- scripts/slurm.py
+++ scripts/slurm.py
@@ -31,18 +31,21 @@ def error(msg: str) -> NoReturn:
 @click.command(
     context_settings=dict(ignore_unknown_options=True, allow_extra_args=True)
 )
-@click.option("--slurm-sleep", "sleep", default=10.0)
-@click.option('--job-name', help='name for job')
-@click.option('--output', help='output filename', type=click.Path(file_okay=True, dir_okay=False))
+@click.option("--job-name", help="name for job")
+@click.option(
+    "--output", help="output filename", type=click.Path(file_okay=True, dir_okay=False)
+)
 @click.option("--script", help="argument is a script")
 @click.pass_context
-def slurm(ctx, sleep: float, script: bool, job_name: str | None, output: str | None):
+def slurm(ctx, script: bool, job_name: str | None, output: str | None):
     sbatch = which("sbatch")
     if sbatch is None:
         error("slurm is not installed!")
     if not ctx.args:
         error("no command to run!")
 
+    if ctx.args[0].startswith("-"):
+        error(f"unknown option: {ctx.args[0]} (try --help)")
 
     if script:
         slurmsh = ctx.args[0]
@@ -51,21 +54,21 @@ def slurm(ctx, sleep: float, script: bool, job_name: str | None, output: str | N
             error(f"{slurmsh} is not a file!")
         remove = False
     else:
-        slurmsh = f".slurm-{uuid4()}.sh"
+        slurmsh = f".{ctx.args[0]}-{uuid4()}-slurm.sh"
         remove = True
         args = []
         with open(slurmsh, "wt", encoding="utf8") as fp:
             print("#!/bin/bash", file=fp)
-            print(f"sleep {sleep}", file=fp)
+            print("sleep 10", file=fp)
             print(shlex.join(ctx.args), file=fp)
 
     try:
         cmds = [sbatch]
-        cmds.append(f'--time=1:00:00') # 1 hour
+        cmds.append("--time=1:00:00")  # 1 hour max
         if job_name:
-            cmds.append(f'--job-name={job_name}')
+            cmds.append(f"--job-name={job_name}")
         if output:
-            cmds.append(f'--output={output}')
+            cmds.append(f"--output={output}")
 
         cmds.extend([slurmsh, *args])
 
