4f1eebc33e7f6059ad5477171b5d2a92e45a6c87
Ian Castleden <ian.castleden@uwa.edu.au>
2025-08-29 16:09:24 +0800

    add JULIA_HOME

--- profile.d/Zjulia.sh
+++ profile.d/Zjulia.sh
@@ -1,7 +1,8 @@
 # /etc/profile.d/Zjulia.sh
 # need the Z so module use is done last
 export PATH=/var/legends/bin:$PATH
-export JULIA_DEPOT_PATH=/var/legends/juliaup
+export JULIAUP_HOME=/var/legends/juliaup
+export JULIA_DEPOT_PATH=$JULIAUP_HOME
 export JULIAUP_DEPOT_PATH=$JULIA_DEPOT_PATH
 if [ "$(type -t module)" = "function" ]; then
     module use -p /var/legends/modules
