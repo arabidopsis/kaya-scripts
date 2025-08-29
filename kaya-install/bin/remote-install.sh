37952bea4a4ea8e892e4b8f621aff8f0fb9a290e
Ian Castleden <ian.castleden@uwa.edu.au>
2025-08-30 04:25:13 +0800

    added script

--- kaya-install/bin/remote-install.sh
+++ kaya-install/bin/remote-install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+if [ "$#" -eq 0 ]; then
+    echo "No machine specified"
+fi
+MACHINE=$1
 TGT=kaya-dist
 rm -rf dist
 uv build
-scp -q -r dist onycis:$TGT
-ssh onycis "PATH=~/.local/bin uv tool uninstall kaya-install"
-ssh onycis "PATH=~/.local/bin uv tool install --find-links $TGT kaya-install && rm -rf $TGT"
+scp -q -r dist $MACHINE:$TGT
+ssh $MACHINE "PATH=~/.local/bin uv tool uninstall kaya-install"
+ssh $MACHINE "PATH=~/.local/bin uv tool install --find-links $TGT kaya-install && rm -rf $TGT"
