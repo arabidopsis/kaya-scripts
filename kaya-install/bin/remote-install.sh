5774dd0fa09ea2f4528238a6799ae109644e98bd
Ian Castleden <ian.castleden@uwa.edu.au>
2025-08-30 17:11:34 +0800

    install kaya

--- kaya-install/bin/remote-install.sh
+++ kaya-install/bin/remote-install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 if [ "$#" -eq 0 ]; then
     echo "No machine specified"
+    exit 1
 fi
 MACHINE=$1
-TGT=kaya-dist
+TGT=kaya-install
 rm -rf dist
 uv build
-scp -q -r dist $MACHINE:$TGT
-ssh $MACHINE "PATH=~/.local/bin uv tool uninstall kaya-install"
-ssh $MACHINE "PATH=~/.local/bin uv tool install --find-links $TGT kaya-install && rm -rf $TGT"
+scp -q -r dist $MACHINE:${TGT}-dist
+ssh $MACHINE "PATH=~/.local/bin uv tool uninstall ${TGT}"
+ssh $MACHINE "PATH=~/.local/bin uv tool install --find-links ${TGT}-dist ${TGT} && rm -rf ${TGT}-dist"
